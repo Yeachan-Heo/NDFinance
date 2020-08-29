00a4441f2a32c1b8ad8cc87381012244add88bc2
Yeachan-Heo <hurrc04@gmail.com>
2020-08-29 05:54:04 +0000

    add install talib

---
+++ install_talib.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+sudo apt install build-essential wget -y
+wget https://artiya4u.keybase.pub/TA-lib/ta-lib-0.4.0-src.tar.gz
+tar -xvf ta-lib-0.4.0-src.tar.gz
+cd ta-lib/
+./configure --prefix=/usr
+make
+sudo make install
