0a0902553a2d12bb68149cd8740acdaf7524aafd
Yeachan-Heo <hurrc04@gmail.com>
2020-09-27 01:10:39 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -10,9 +10,9 @@ setup(
               'ndfinance.analysis', 'ndfinance.analysis.backtest', 'ndfinance.analysis.technical',
               'ndfinance.analysis.machine_learning', 'ndfinance.analysis.machine_learning.rl_envs',
               'ndfinance.callbacks', 'ndfinance.strategies', 'ndfinance.strategies.basic', 'ndfinance.strategies.trend', 'ndfinance.strategies.utils', 'ndfinance.visualizers'],
-    url='',
-    license='',
+    url='https://github.com/Yeachan-Heo/NDFinance.git',
+    license='MIT',
     author='Yeachan-Heo',
-    author_email='',
-    description=''
+    author_email='rlstart@kakao.com',
+    description='Backtest & ML/DL Quant analysis'
 )
